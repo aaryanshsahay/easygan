21208b104b1d56d8c3302e5a351470832eb43a7c
Aaryansh Sahay <53135035+aaryanshsahay@users.noreply.github.com>
2023-07-23 15:48:50 +0530

    Update dcgan.py

--- models/dcgan.py
+++ models/dcgan.py
@@ -20,7 +20,7 @@ import tensorflow as tf
 
 from keras.layers import (Dropout, Input, Dense, Conv2D,
 						  MaxPooling2D, GlobalAveragePooling2D,
-						  Upsampling2D, Conv2DTranspose, Reshape,
+						  UpSampling2D, Conv2DTranspose, Reshape,
 						  Flatten, Activation, BatchNormalization,
 						  LeakyReLU, ReLU)
 
